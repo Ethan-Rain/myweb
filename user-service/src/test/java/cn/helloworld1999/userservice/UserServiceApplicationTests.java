b7767469f00d3448cf2f2608bf80f9adf1018dea
EthanRui <1327803942@qq.com>
2025-05-11 10:46:50 +0800

    feat(web-crawler): 添加下载失败任务队列并更新相关服务依赖
    - 在 MessageListener 中添加下载失败任务队列，记录失败的下载任务
    - 更新 media-service 和 user-service 的 pom 文件，添加 common 模块依赖
    - 在 pom.xml 中添加 spring-boot-test 依赖
    - 修复 UserServiceApplicationTests 中的注解和方法格式

--- user-service/src/test/java/cn/helloworld1999/userservice/UserServiceApplicationTests.java
+++ user-service/src/test/java/cn/helloworld1999/userservice/UserServiceApplicationTests.java
@@ -1,13 +1,13 @@
 package cn.helloworld1999.userservice;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class UserServiceApplicationTests {
+public class UserServiceApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
     }
 
 }
