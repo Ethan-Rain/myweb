ea66214946930d3189c49c1e33ace1232efef61c
EthanRui <1327803942@qq.com>
2025-05-16 01:27:39 +0800

    feat(common): 实现 AES 和 RSA 加密策略
    - 新增 AESCryptoStrategy 和 RSACryptoStrategy 类实现 CryptoStrategy 接口
    - 添加 CryptoContext 类用于选择和执行加密策略
    - 创建 CryptoRequest 和 CryptoResponse DTO 类用于封装加密请求和响应数据
    - 实现 CryptoUtil 工具类提供静态加密方法
    - 定义 AsymmetricCryptoStrategy 接口用于非对称加密策略

---
+++ common/src/main/java/cn/helloworld1999/common/util/strategy/AESCryptoStrategy.java
@@ -0,0 +1,28 @@
+package cn.helloworld1999.common.util.strategy;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
+
+public class AESCryptoStrategy implements CryptoStrategy {
+
+    private static final String ALGORITHM = "AES";
+
+    @Override
+    public String encrypt(String data, String key) throws Exception {
+        SecretKeySpec secretKey = new SecretKeySpec(key.getBytes(), ALGORITHM);
+        Cipher cipher = Cipher.getInstance(ALGORITHM);
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+        byte[] encryptedData = cipher.doFinal(data.getBytes());
+        return Base64.getEncoder().encodeToString(encryptedData);
+    }
+
+    @Override
+    public String decrypt(String encryptedData, String key) throws Exception {
+        SecretKeySpec secretKey = new SecretKeySpec(key.getBytes(), ALGORITHM);
+        Cipher cipher = Cipher.getInstance(ALGORITHM);
+        cipher.init(Cipher.DECRYPT_MODE, secretKey);
+        byte[] decryptedData = cipher.doFinal(Base64.getDecoder().decode(encryptedData));
+        return new String(decryptedData);
+    }
+}
---
+++ common/src/main/java/cn/helloworld1999/common/util/strategy/AsymmetricCryptoStrategy.java
@@ -0,0 +1,5 @@
+package cn.helloworld1999.common.util.strategy;
+
+public interface AsymmetricCryptoStrategy extends CryptoStrategy {
+    // 可以在这里定义特有的非对称加密方法
+}
